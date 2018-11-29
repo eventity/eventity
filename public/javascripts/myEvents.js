e1c081c7320576d357020446d260771e65e4505d
Bianca Castro <biiianca_00@hotmail.com>
2018-11-29 12:58:04 +0100

    new delete buttonin my events-only style

--- public/javascripts/myEvents.js
+++ public/javascripts/myEvents.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const elems = document.querySelectorAll('.sidenav');
   const instance   = M.Sidenav.init(elems);
+
+  document.querySelector('.delete-event-link')
+
 });
