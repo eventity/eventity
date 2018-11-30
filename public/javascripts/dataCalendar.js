5318f4974e8c537f4a5173952d3535623efba43d
David Diez <ddiezr@gmail.com>
2018-11-30 13:25:39 +0100

    Added Autocomplete esay jquery

--- public/javascripts/dataCalendar.js
+++ public/javascripts/dataCalendar.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const picker = document.querySelectorAll('.datepicker');
   const instances = M.Datepicker.init(picker, {
-    format:'yyyy-mm-dd',
+    format: 'yyyy-mm-dd',
     setDefaultDate: true,
     defaultDate: new Date(),
   });
